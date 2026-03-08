2b194bdcb6b996cf97933a2a802fed777bc419d8
jadonsamuel08 <you@example.com>
2026-03-08 16:18:34 -0400

    Improved number guesser functionality

--- number-guesser/number_guesser.py
+++ number-guesser/number_guesser.py
@@ -1,22 +1,33 @@
 import random
 
 def main():
-    print("="*56 + "\n\tWELCOME TO NUMBER GUESSER BY JADON SAMUEL\n" + "="*56 , end="\n\n")
-    print("RULES: A random number between 1 and 100 will be generated. You're goal is to guess that number. You have infinite tries.")
-    
-    num_guesses = 1
-    rand_num = random.randint(1,100)
-    
+    print("=" * 56 + "\n\tWELCOME TO NUMBER GUESSER BY JADON SAMUEL\n" + "=" * 56, end="\n\n")
+    print("RULES: A random number between 1 and 100 will be generated. Your goal is to guess that number. You have infinite tries.\n")
+
     while True:
-        try:
-            guess = int(input(f"Guess #{num_guesses}: "))
-            if guess == rand_num:
-                print("Congradulations You Won!!!")
-                break
-            else:
-                print("Incorrect. Try again.")
-                num_guesses += 1
-        except Exception as e:
-            print("Please enter a number between 1 and 100")
-        
+        num_guesses = 1
+        rand_num = random.randint(1, 100)
+
+        while True:
+            try:
+                guess = int(input(f"Guess #{num_guesses}: "))
+                if guess < 1 or guess > 100:
+                    print("Please enter a number between 1 and 100.")
+                elif guess == rand_num:
+                    print(f"Congratulations! You guessed it in {num_guesses} guess{'es' if num_guesses > 1 else ''}!")
+                    break
+                elif guess < rand_num:
+                    print("Too low! Try again.")
+                    num_guesses += 1
+                else:
+                    print("Too high! Try again.")
+                    num_guesses += 1
+            except ValueError:
+                print("Invalid input. Please enter a whole number.")
+
+        play_again = input("\nPlay again? (y/n): ").strip().lower()
+        if play_again != "y":
+            print("Thanks for playing!")
+            break
+
 main()
